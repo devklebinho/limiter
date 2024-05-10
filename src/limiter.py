119b29718cf549f0ab45e0e3a336736ce420e1c7
devklebinho <contato@klebersilva.dev.br>
2024-05-10 16:36:52 -0300

    finish

--- src/limiter.py
+++ src/limiter.py
@@ -1,18 +1,41 @@
 from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
+from comtypes import CoInitialize, CoUninitialize, CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
+# Inicializa o COM
+CoInitialize()
+
+# Obtém o dispositivo de áudio apenas uma vez para evitar repetidas chamadas
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume_interface = cast(interface, POINTER(IAudioEndpointVolume))
+
+def get_system_volume():
+    return volume_interface.GetMasterVolumeLevelScalar()
+
 def set_system_volume(volume):
-    devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(
-        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-    volume_interface = cast(interface, POINTER(IAudioEndpointVolume))
     volume_interface.SetMasterVolumeLevelScalar(volume, None)
 
 def main():
-    # Defina o volume desejado entre 0.0 e 1.0
-    volume = 0.0  # por exemplo, 0.5 representa 50%
-    set_system_volume(volume)
+    try:
+        # Defina o volume desejado entre 0.0 e 1.0
+        allowVolume = 0.4  # por exemplo, 40%
+        set_system_volume(allowVolume)
+        
+        while True:
+            current_volume = get_system_volume()
+            if current_volume > 0.41:
+                set_system_volume(allowVolume)
+                #print("Volume ajustado para:", allowVolume)
+            else:
+                #print("Volume Ok")
+                pass
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Libera o COM
+        CoUninitialize()
 
 if __name__ == "__main__":
     main()
