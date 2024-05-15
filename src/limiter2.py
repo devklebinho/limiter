aeff6fa7a44382586d370551d8a613d77d5a726a
devklebinho <contato@klebersilva.dev.br>
2024-05-15 18:03:57 -0300

    new version v0.0.3

---
+++ src/limiter2.py
@@ -0,0 +1,71 @@
+from ctypes import cast, POINTER
+from comtypes import CoInitialize, CoUninitialize, CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import psutil
+import subprocess
+import time
+
+
+
+# Inicializa o COM
+CoInitialize()
+
+# Obtém o dispositivo de áudio apenas uma vez para evitar repetidas chamadas
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume_interface = cast(interface, POINTER(IAudioEndpointVolume))
+
+def get_system_volume():
+    return volume_interface.GetMasterVolumeLevelScalar()
+
+def set_system_volume(volume):
+    volume_interface.SetMasterVolumeLevelScalar(volume, None)
+
+def is_program_running(program_name):
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == program_name:
+            return True
+    return False
+    
+
+def main():
+    try:
+        # Defina o volume desejado entre 0.0 e 1.0
+        allowVolume = 0.4  # por exemplo, 40%
+        set_system_volume(allowVolume)
+        
+        while True:
+            current_volume = get_system_volume()
+            if current_volume > 0.41:
+                set_system_volume(allowVolume)
+                #print("Volume ajustado para:", allowVolume)
+            else:
+                #print("Volume Ok")
+                pass
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Libera o COM
+        CoUninitialize()
+
+#    program_name = 'CScliente.exe'
+#    while True:
+#        try:
+#            if is_program_running(program_name):
+#                print(f'O programa "{program_name}" está em execução.')
+#                
+#            else:
+#                print(f'O programa "{program_name}" não está em execução.')
+#                time.sleep(3)
+#                program_path = r"reiniciar.bat"  
+#                print("Reiniciando o Sistema...")
+#                subprocess.run(program_path)
+#            time.sleep(3)
+#        except Exception as e:
+#            print(f'Ocorreu uma exceção: {e}')
+#            break
+
+
+if __name__ == "__main__":
+    main()
